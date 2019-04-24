e49ca194074db04362965d54b26faa6f33ae813c
smingolelli <smingolelli@stetson.edu>
2019-04-24 14:54:41 -0400

    Fixed methods in header

--- database.h
+++ database.h
@@ -16,20 +16,8 @@ class Database{
         int lastid=0;
         Database(){ context = redisConnect("localhost",6379); } //default constructor
         //methods
-        void write(std::string key, std::string value){
-            redisReply *reply;
-            reply = (redisReply*) redisCommand(context,"SET %s %s", key.c_str(), value.c_str());
-            freeReplyObject(reply);
-        }
-        std::string read(std::string key) const{
-            redisReply *reply;
-            reply = (redisReply*) redisCommand(context,"GET %s", key.c_str());
-            std::string s(reply->str);
-            std::cout << s;
-            freeReplyObject(reply);
-            return s;
-
-        }
+        void write(std::string key, std::string value); 
+        std::string read(std::string key) const;
         void read_lastid(int id) const;
         void increment_lastid();
 };
