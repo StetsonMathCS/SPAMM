f2c2be13bf28ac1cfa7e8183d7509d4cd913d164
Derrek Woodworth <dwoodworth@stetson.edu>
2019-04-30 10:49:26 -0400

    Adding stopping function

--- game_server.h
+++ game_server.h
@@ -18,49 +18,50 @@
 #include <string>
 class Player;
 struct user_data{
-	// Name of the user
-	char *name;
-	//ID of the user, will only be set on a sucessful log on
-	int id;
+    // Name of the user
+    char *name;
+    //ID of the user, will only be set on a sucessful log on
+    int id;
     //Player pointer
     Player *player;
     //Stores the state of the player log on sequence
     int state;
-	//Each user has a socket that is their network connection
-	SOCKET sock;
-	//Telnet struct that is the connection of the user, essentially an object, it helps abstract the details of telnet functionality
-	telnet_t *telnet;
-	//Libtelnet sends and recieves messages as chars using a buffer
-	char linebuffer[256];
-	int linepos;
+    //Each user has a socket that is their network connection
+    SOCKET sock;
+    //Telnet struct that is the connection of the user, essentially an object, it helps abstract the details of telnet functionality
+    telnet_t *telnet;
+    //Libtelnet sends and recieves messages as chars using a buffer
+    char linebuffer[256];
+    int linepos;
 };
 class GameServer {
-	private:
-		//Write out the structs that store data within the class, these are only relevant within the class implementation
-		//Store the connection information of a user  who connect to the server, only keeping track of the telnet necessary information
-		//Contains the polling information for the sockets that the users are connected on
-		struct pollfd *pfd;
-		//Store the user data including the telnet session and the socket used for communication
-		struct user_data *users;
-		//Socket where the program listens for incoming connections
-		SOCKET listen_sock;	
-		//A middle socket structure that ties the network aspect(IPV4 and port number) into sockets which the application can communicate with
-		struct sockaddr_in addr;
-		//The IPV4 port to address the server from on the network
-		int portNum;
-		//The Return status of low level functions
-		int rs;
-	public:
-		GameServer(int portNumber);
-		GameServer();
-		//This function is called whenever a user enters a string, the int is the users id
-		void setCallBackFunction(void (*f)(Player*, std::string));
-		//Function that will validate the user's log on information, returns -1 if invalid, else return their id
-		void setLogOnFunction(int (*f)(std::string, std::string));
+    private:
+        //Write out the structs that store data within the class, these are only relevant within the class implementation
+        //Store the connection information of a user  who connect to the server, only keeping track of the telnet necessary information
+        //Contains the polling information for the sockets that the users are connected on
+        struct pollfd *pfd;
+        //Store the user data including the telnet session and the socket used for communication
+        struct user_data *users;
+        //Socket where the program listens for incoming connections
+        SOCKET listen_sock;	
+        //A middle socket structure that ties the network aspect(IPV4 and port number) into sockets which the application can communicate with
+        struct sockaddr_in addr;
+        //The IPV4 port to address the server from on the network
+        int portNum;
+        //The Return status of low level functions
+        int rs;
+    public:
+        GameServer(int portNumber);
+        GameServer();
+        //This function is called whenever a user enters a string, the int is the users id
+        void setCallBackFunction(void (*f)(Player*, std::string));
+        //Function that will validate the user's log on information, returns -1 if invalid, else return their id
+        void setLogOnFunction(int (*f)(std::string, std::string));
         void setCreateNewUserFunction(int (*f)(std::string, std::string));
-		int getPortNumber() const;
-		void start();
-		void printToUser(Player*, std::string);
+        int getPortNumber() const;
+        void start();
+        void stop();
+        void printToUser(Player*, std::string);
         void printToUsers(std::vector<Player*>, std::string);
         void broadcast(std::string);
 };
